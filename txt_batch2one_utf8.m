b2af61f51768c50afd36b3775f8e171645b84554
weify627 <542113870@qq.com>
2017-04-18 21:05:42 +0800

    Create txt_batch2one_utf8.m

---
+++ txt_batch2one_utf8.m
@@ -0,0 +1,27 @@
+classlist  = dir('D:\本学期课程\机器学习概论\new_weibo_13638');
+% 读取后namelist 的格式为
+% name -- filename
+% date -- modification date
+% bytes -- number of bytes allocated to the file
+% isdir -- 1 if name is a directory and 0 if not
+result_id=fopen('result.txt','wt');
+l = length(classlist); 
+maxlen=0;
+
+ for i=3:l
+        file_name{i}=classlist(i).name;
+        entrylist  = dir(['D:\本学期课程\机器学习概论\new_weibo_13638\' file_name{i}]);
+        if maxlen<length(entrylist)
+            maxlen=length(entrylist);
+            record=[num2str(i) num2str(j)]
+        end
+            
+        for j=3:length(entrylist)
+            x= importdata(['D:\本学期课程\机器学习概论\new_weibo_13638\' file_name{i} '\' entrylist(j).name]);
+            fid=fopen(['D:\本学期课程\机器学习概论\new_weibo_13638\' file_name{i} '\' entrylist(j).name],'r+','n','utf-8');
+            tline=fgetl(fid); %bb = textscan(fid,'%s');
+            fprintf(result_id,'n,%d,%s\n',i-3,tline);
+            fclose(fid);
+        end
+ end
+fclose(result_id);
